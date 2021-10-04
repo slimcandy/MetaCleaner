1b7f4179c706a7b990ce7914daffb6761710a724
SlimCandy <cgpnrjmhppeb@ro.ru>
2021-10-04 21:58:30 +0400

    Add exiftool

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import fileupload from 'express-fileupload'
+import { execFile } from 'child_process'
+import exiftool from 'node-exiftool'
+import exiftoolBin from 'dist-exiftool'
 
 const PORT = process.env.PORT || 3001
 
@@ -11,7 +14,23 @@ app.post('/upload', (req, res, next) => {
   try {
     const file = req.files.file
     if (file) {
-      res.json(file)
+      // res.json(file)
+      const exiftoolProcess = new exiftool.ExiftoolProcess(exiftoolBin)
+
+      exiftoolProcess
+        .open()
+        // display pid
+        .then((pid) => console.log('Started exiftool process %s', pid))
+        .then(() => exiftoolProcess.readMetadata('image.jpeg', ['-File:all']))
+        .then((data, error) => {
+          if (error && error.length > 0) {
+            res.json(`error: ${error}`)
+          }
+          res.json(data)
+        })
+        .catch((error) => {
+          res.json(`error: ${error}`)
+        })
     } else {
       res.status(400).send({
         status: false,
