4af99ac8e61091d4e752d210d2acf88fc890d099
SlimCandy <cgpnrjmhppeb@ro.ru>
2021-11-09 01:42:01 +0300

    Add share button

--- client/src/components/DownloadBlock/DownloadBlock.tsx
+++ client/src/components/DownloadBlock/DownloadBlock.tsx
@@ -1,5 +1,6 @@
+import { useState, useEffect } from 'react'
 import { IDownloadBlockProps } from './types'
-import { IMetaData } from '../../appTypes'
+import { IMetaData, IMetaFileInfo } from '../../appTypes'
 
 const exifTitleMaxLength = 3
 
@@ -16,7 +17,27 @@ const downloadIcon = (
   </svg>
 )
 
-const parseTitleFromExifObject = (obj: IMetaData, limit = 3) => {
+const shareIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='16'
+    height='16'
+    fill='currentColor'
+    className='bi bi-box-arrow-up'
+    viewBox='0 0 16 16'
+  >
+    <path
+      fillRule='evenodd'
+      d='M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z'
+    />
+    <path
+      fillRule='evenodd'
+      d='M7.646.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 1.707V10.5a.5.5 0 0 1-1 0V1.707L5.354 3.854a.5.5 0 1 1-.708-.708l3-3z'
+    />
+  </svg>
+)
+
+const parseTitleFromExifObject = (obj: IMetaData, limit = 3): string => {
   let resultString = ''
   const objLength = Object.keys(obj).length
   const objKeys = Object.keys(obj)
@@ -38,7 +59,27 @@ const parseTitleFromExifObject = (obj: IMetaData, limit = 3) => {
   return resultString
 }
 
+const shareAction = (file: IMetaFileInfo): Promise<void> => {
+  const navigatorHack: any = window.navigator
+  return fetch(file.link)
+    .then((response) => response.blob())
+    .then((fileBlob) => {
+      const fileToShare = new File([fileBlob], file.name, {
+        type: file.mimetype,
+      })
+      return navigatorHack.share({ files: [fileToShare] })
+    })
+}
+
 const DownloadBlock = (props: IDownloadBlockProps): JSX.Element => {
+  const [canIShareFile, setCanIShareFile] = useState(false)
+  useEffect(() => {
+    const navigatorHack: any = window.navigator
+    if (navigatorHack && navigatorHack.share && navigatorHack.canShare) {
+      setCanIShareFile(true)
+    }
+  }, [])
+
   if (!props.data) {
     return (
       <small className='form-text'>
@@ -63,14 +104,27 @@ const DownloadBlock = (props: IDownloadBlockProps): JSX.Element => {
   }
   return (
     <>
-      <div className='d-grid '>
+      <div className='d-flex'>
         <a
           href={fileInfo.link}
           title={fileInfo.name}
-          className='btn btn-primary btn-lg'
+          className={`btn btn-primary btn-lg ${
+            canIShareFile ? 'col-8' : 'col-12'
+          }`}
+          type={fileInfo.mimetype}
+          download
         >
           {downloadIcon} Download
         </a>
+        {canIShareFile && (
+          <button
+            type='button'
+            className='btn btn-link col-4'
+            onClick={() => shareAction(fileInfo)}
+          >
+            or share it {shareIcon}
+          </button>
+        )}
       </div>
       {exifNumberAfter - exifNumberBefore < 0 && (
         <small className='form-text'>
