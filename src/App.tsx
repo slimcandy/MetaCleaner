60c61eff93d5c00abbc82b6386e3707d9ce79e3f
SlimCandy <cgpnrjmhppeb@ro.ru>
2023-04-15 17:13:27 +0200

    Refactor form getters & add semantic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useId, useState } from "react";
 
 function App() {
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
   const [status, setStatus] = useState("");
+  const [previewUrl, setPreviewUrl] = useState("");
 
-  const sendEmail = async () => {
+  const emailId = useId();
+  const subjectId = useId();
+  const messageId = useId();
+
+  async function sendEmail(event: React.FormEvent) {
+    event.preventDefault();
+
     try {
       setStatus("Sending...");
       const response = await fetch("/.netlify/functions/send-email", {
@@ -17,41 +24,92 @@ function App() {
 
       if (response.ok) {
         const data = await response.json();
-        console.log("Email sent:", data);
-        setStatus("Email sent successfully!");
+        setStatus("Email sent successfully: " + JSON.stringify(data));
+        setPreviewUrl(data.previewUrl);
       } else {
-        setStatus("Error sending email.");
+        setStatus(
+          "Error sending email: " +
+            response.statusText +
+            " " +
+            response.status +
+            " " +
+            response.body
+        );
       }
     } catch (error) {
-      console.error("Error sending email:", error);
-      setStatus("Error sending email.");
+      setStatus("Error sending email: " + JSON.stringify(error));
     }
-  };
+  }
+
+  function onEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
+    return setEmail(event.target.value);
+  }
+  function onSubjectChange(event: React.ChangeEvent<HTMLInputElement>) {
+    return setSubject(event.target.value);
+  }
+  function onMessageChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    return setMessage(event.target.value);
+  }
 
   return (
     <>
-      <input
-        className="input"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <input
-        className="input"
-        placeholder="Subject"
-        onChange={(e) => setSubject(e.target.value)}
-        value={subject}
-      />
-      <textarea
-        className="input"
-        placeholder="Message"
-        onChange={(e) => setMessage(e.target.value)}
-        value={message}
-      />
-      <button onClick={sendEmail} className="button">
-        Send Email
-      </button>
-      <div className="status">{status}</div>
+      <h1>Send an Email</h1>
+      <form onSubmit={sendEmail}>
+        <dl>
+          <dt>
+            <label htmlFor={emailId}>Email</label>
+          </dt>
+          <dd>
+            <input
+              type="email"
+              placeholder="john@example.org"
+              onChange={onEmailChange}
+              value={email}
+              id={emailId}
+            />
+          </dd>
+          <dt>
+            <label htmlFor={subjectId}>Subject</label>
+          </dt>
+          <dd>
+            <input
+              type="text"
+              placeholder="Subject"
+              onChange={onSubjectChange}
+              value={subject}
+              id={subjectId}
+            />
+          </dd>
+          <dt>
+            <label htmlFor={messageId}>Message</label>
+          </dt>
+          <dd>
+            <textarea
+              placeholder="Message"
+              onChange={onMessageChange}
+              value={message}
+              id={messageId}
+            />
+          </dd>
+        </dl>
+        <button type="submit">Send Email</button>
+      </form>
+
+      {status.length > 0 && (
+        <>
+          <hr />
+
+          <h2>Status</h2>
+          <samp>{status}</samp>
+
+          <h2>Preview Email</h2>
+          {previewUrl.length > 0 && (
+            <a href={previewUrl} target="_blank" rel="noreferrer">
+              {previewUrl}
+            </a>
+          )}
+        </>
+      )}
     </>
   );
 }
