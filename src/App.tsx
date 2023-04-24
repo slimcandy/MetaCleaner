67817f6cfa3b403a099da39950e0b49d9836bde1
SlimCandy <cgpnrjmhppeb@ro.ru>
2023-04-24 11:37:53 +0200

    Style form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-var */
 import React, { useId, useRef } from "react";
+import "./App.css";
 
 function handleDragEvents(event: React.DragEvent<HTMLDivElement>) {
   event.preventDefault();
@@ -59,21 +60,54 @@ function App(): JSX.Element {
   }
 
   return (
-    <>
-      <h1>Send File</h1>
-      <form>
+    <div className="prose mx-auto rounded-lg shadow-2xl bg-white border-2 border-slate-300">
+      <h1 className="border-b-2 border-slate-300 px-6 py-7 w-full">
+        Remove metadata from images
+      </h1>
+      <form className="py-4 px-6">
         <div
           ref={dropArea}
           onDragEnter={handleDragEvents}
           onDragOver={handleDragEvents}
           onDragLeave={handleDragEvents}
           onDrop={handleDrop}
+          className="border-2 border-dashed border-slate-300 rounded-lg px-6 py-7 w-full text-center cursor-pointer hover:border-slate-400 transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-slate-400 focus:border-transparent hover:shadow-xl hover:bg-slate-100 active:bg-slate-200 active:shadow-none active:border-transparent"
         >
-          <p>Drag and drop your picture here</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="mx-auto h-12 w-12 text-slate-500"
+            viewBox="0 0 16 16"
+          >
+            <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+            <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z" />
+          </svg>
+          <h3 className="mt-2 text-base font-medium text-slate-900">
+            Drop your images here
+          </h3>
+          <p className="mt-1 text-sm text-slate-500">
+            We&apos;ll remove all metadata from your images
+          </p>
         </div>
-
-        <label htmlFor={fileId}>or Browse</label>
+        <label
+          htmlFor={fileId}
+          className="mt-6 block text-base font-medium text-slate-900"
+        >
+          Or browse
+        </label>
         <input
+          className="block w-full text-sm text-slate-500 my-2 py-2 px-4
+          rounded border-2 border-slate-300 bg-slate-50
+          file:mr-4 file:py-2 file:px-4
+          file:rounded file:border-2 file:border-slate-300
+          file:font-semibold
+          file:slate-900 file:hover:border-slate-400 file:transition-all file:duration-300 file:ease-in-out
+          file:focus:outline-none file:focus:ring-2 file:focus:ring-slate-400 file:focus:border-transparent
+          file:hover:shadow-xl file:hover:bg-slate-100 file:hover:cursor-pointer
+          file:active:bg-slate-200 file:active:shadow-none file:active:border-transparent
+        "
           id={fileId}
           type="file"
           accept="image/*"
@@ -83,9 +117,11 @@ function App(): JSX.Element {
           multiple
           hidden
         />
-        <button type="submit">Send</button>
+        <button type="submit" className="sr-only">
+          Send
+        </button>
       </form>
-    </>
+    </div>
   );
 }
 
