a627f987d1f6647e66ec9952f59aee334aecddfe
SlimCandy <cgpnrjmhppeb@ro.ru>
2023-04-26 14:36:56 +0200

    Add vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,42 @@
 import react from '@vitejs/plugin-react';
-// import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default {
   plugins: [
-    react()
-    // VitePWA({
-    //   manifest: './src/app.webmanifest',
-    //   sw: './src/sw/sw-register.ts',
-    //   strategies: 'injectManifest',
-    //   injectRegister: false,
-    //   workbox: {
-    //     swSrc: './src/sw/sw-register.ts',
-    //     swDest: 'sw.js'
-    //   }
-    // })
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['/icons/favicon.ico', '/icons/apple-touch-icon.png'],
+
+      manifest: {
+        name: 'MetaCleaner, Remove Metadata from Images',
+        description:
+          'Easily remove metadata from your images with our online tool, MetaCleaner. Preserve your privacy by stripping out EXIF data from your photos in seconds.',
+        short_name: 'Meta',
+        start_url: '/',
+
+        theme_color: '#CBD5E1',
+        display: 'standalone',
+
+        icons: [
+          {
+            src: '/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/android-chrome-384x384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
   ]
-  // root: './public',
-  // publicDir: 'public',
-  // build: {
-  // outDir: 'dist',
-  // rollupOptions: {
-  // input: {
-  // main: 'public/index.html',
-  // sw: 'src/sw/sw-register.ts'
-  // }
-  // }
-  // }
 };
