270d675ca9f3d6e1190c114013cb06e4a6a253a0
SlimCandy <cgpnrjmhppeb@ro.ru>
2023-04-29 16:02:18 +0200

    Add download section

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,53 +1,68 @@
+import { CleanerResult, ShareObject } from './types';
+
 export function handleDragEvents(event: React.DragEvent<HTMLDivElement>): void {
   event.preventDefault();
   event.stopPropagation();
 }
 
-export function removeMetaData(file: File): void {
-  const img = new Image();
-  img.src = URL.createObjectURL(file);
+export async function removeMetaData(file: File): Promise<CleanerResult> {
+  return new Promise(resolve => {
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
 
-  img.onload = function imageOnLoad() {
-    const canvas = document.createElement('canvas');
-    canvas.width = img.width;
-    canvas.height = img.height;
-    const ctx = canvas.getContext('2d');
-    ctx?.drawImage(img, 0, 0);
+    img.onload = function imageOnLoad() {
+      const canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext('2d');
+      ctx?.drawImage(img, 0, 0);
 
-    // Convert canvas to a Blob without metadata
-    canvas.toBlob(function saveFile(blob) {
-      if (blob === null) return;
+      // Convert canvas to a Blob without metadata
+      canvas.toBlob(function saveFile(blob) {
+        if (blob === null) return;
 
-      // Save the Blob as a file
-      const anchor = document.createElement('a');
-      anchor.href = URL.createObjectURL(blob);
-      anchor.download = file.name;
-      anchor.style.display = 'none';
-      document.body.appendChild(anchor);
-      anchor.click();
-      document.body.removeChild(anchor);
-      URL.revokeObjectURL(anchor.href);
-    }, file.type);
-    // Revoke object URL to free memory
-    URL.revokeObjectURL(img.src);
-  };
+        const downloadLink = URL.createObjectURL(blob);
+        const imageName = file.name;
+        const imageFile = new File([blob], imageName, { type: file.type });
+
+        resolve({ src: img.src, downloadLink, file: imageFile });
+        // Revoke object URL to free memory
+        URL.revokeObjectURL(img.src);
+      }, file.type);
+    };
+  });
 }
 
-export function handleFiles(files: FileList = new DataTransfer().files): void {
+export async function handleFiles(
+  files: FileList = new DataTransfer().files
+): Promise<CleanerResult[]> {
+  const results = [];
   for (let i = 0; i < files.length; i++) {
-    removeMetaData(files[i]);
+    const result = await removeMetaData(files[i]);
+    results.push(result);
+  }
+  return results;
+}
+
+export async function shareImage(file: File): Promise<void> {
+  const check = canIShareImage(file);
+  if (check) {
+    try {
+      await navigator.share({ files: [file] });
+    } catch (error) {
+      console.error('Sharing failed:', error);
+    }
+  } else {
+    console.error('Web Share API is not supported in your browser.');
   }
 }
 
-export function handleDrop(event: React.DragEvent<HTMLDivElement>): void {
-  handleDragEvents(event);
-  handleFiles(event.dataTransfer.files);
-}
-
-export function handleFilePickerChange(
-  event: React.ChangeEvent<HTMLInputElement>
-): void {
-  if (event.target.files != null) {
-    handleFiles(event.target.files);
-  }
+export function canIShareImage(file: File): boolean {
+  return (
+    'canShare' in window.navigator &&
+    window.navigator.canShare({
+      files: [file],
+      title: file.name
+    })
+  );
 }
